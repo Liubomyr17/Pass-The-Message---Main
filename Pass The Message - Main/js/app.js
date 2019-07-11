b7a8937920ce3a3851a5ab2e64f721d6a94a344d
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2019-07-11 17:31:33 +0300

    Add files via upload

---
+++ Pass The Message - Main/js/app.js
@@ -0,0 +1,20 @@
+(function () {
+
+    document.getElementById('message-form').addEventListener('submit',
+        function (e) {
+            e.preventDefault();
+            const message = document.getElementById('message');
+            const value = message.value;
+
+            if (value === "") {
+            const feedback = document.querySelector(".feedback");  
+            feedback.classList.add("show"); 
+            setTimeout(function(){
+                feedback.classList.remove("show");
+            },2000);   
+            } else {
+                document.querySelector(".message-content").textContent = value;
+                message.value = "";
+            }
+        });
+})();
